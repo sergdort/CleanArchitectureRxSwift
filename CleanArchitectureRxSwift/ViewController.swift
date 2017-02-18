de13213a3dc1d6c516fc67e65ed3d9355e662009
sergdort <sergdort@gmail.com>
2017-02-18 17:36:20 +0000

    Initial Commit

---
+++ CleanArchitectureRxSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CleanArchitectureRxSwift
+//
+//  Created by sergdort on 18/02/2017.
+//  Copyright © 2017 sergdort. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
