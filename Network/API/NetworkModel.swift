2f99c1a11077a5893cd41171af80aa6057a74004
Andrey Yastrebov <andrey.yastrebov@corp.mail.ru>
2017-03-10 11:56:58 +0300

    - add API template

---
+++ Network/API/NetworkModel.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkModel.swift
+//  CleanArchitectureRxSwift
+//
+//  Created by Andrey Yastrebov on 10.03.17.
+//  Copyright © 2017 sergdort. All rights reserved.
+//
+
+import Foundation
+import RxAlamofire
+import RxSwift
+
+class NetworkModel {
+
+}
