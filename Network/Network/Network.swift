c2586fe0437369a63ac2f839f4af501d136cca1e
Andrey Yastrebov <ayastrebov@gmail.com>
2017-03-17 02:35:45 +0300

    - add network scheduler - fix realm save (wait until done: NO)

--- Network/Network/Network.swift
+++ Network/Network/Network.swift
@@ -16,9 +16,11 @@ import ObjectMapper
 public final class Network<T: BaseMappable> {
 
     private let endPoint: String
+    private let scheduler: ConcurrentDispatchQueueScheduler
 
     required public init(_ endPoint: String) {
         self.endPoint = endPoint
+        self.scheduler = ConcurrentDispatchQueueScheduler(qos: DispatchQoS(qosClass: DispatchQoS.QoSClass.background, relativePriority: 1))
     }
 
     func getItems(_ path: String) -> Observable<[T]> {
@@ -26,6 +28,7 @@ public final class Network<T: BaseMappable> {
         return RxAlamofire
             .json(.get, absolutePath)
             .debug()
+            .observeOn(scheduler)
             .map({ json -> [T] in
                 return Mapper<T>().mapArray(JSONObject: json)!
             })
@@ -36,6 +39,7 @@ public final class Network<T: BaseMappable> {
         return RxAlamofire
             .request(.get, absolutePath)
             .debug()
+            .observeOn(scheduler)
             .map({ json -> T in
                 return Mapper<T>().map(JSONObject: json)!
             })
@@ -46,6 +50,7 @@ public final class Network<T: BaseMappable> {
         return RxAlamofire
             .request(.post, absolutePath, parameters: parameters)
             .debug()
+            .observeOn(scheduler)
             .map({ json -> T in
                 return Mapper<T>().map(JSONObject: json)!
             })
@@ -56,6 +61,7 @@ public final class Network<T: BaseMappable> {
         return RxAlamofire
             .request(.put, absolutePath, parameters: parameters)
             .debug()
+            .observeOn(scheduler)
             .map({ json -> T in
                 return Mapper<T>().map(JSONObject: json)!
             })
@@ -66,6 +72,7 @@ public final class Network<T: BaseMappable> {
         return RxAlamofire
             .request(.delete, absolutePath)
             .debug()
+            .observeOn(scheduler)
             .map({ json -> T in
                 return Mapper<T>().map(JSONObject: json)!
             })
