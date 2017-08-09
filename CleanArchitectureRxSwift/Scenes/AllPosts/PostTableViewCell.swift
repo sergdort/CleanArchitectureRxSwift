5f0ba9079369ace095448d39e18fd11eb0a421d7
Stefano Mondino <stefano.mondino.dev@gmail.com>
2017-08-09 09:10:27 +0200

    Use PostItemViewModel instead of direct Post binding to the UITableViewCell

--- CleanArchitectureRxSwift/Scenes/AllPosts/PostTableViewCell.swift
+++ CleanArchitectureRxSwift/Scenes/AllPosts/PostTableViewCell.swift
@@ -3,4 +3,10 @@ import UIKit
 final class PostTableViewCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var detailsLabel: UILabel!
+    
+    func bind(to viewModel:PostItemViewModel) {
+        self.titleLabel.text = viewModel.title
+        self.detailsLabel.text = viewModel.subtitle
+    }
+    
 }
