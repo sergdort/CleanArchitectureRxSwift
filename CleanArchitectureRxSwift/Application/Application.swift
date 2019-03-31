cce1f7f72b8680674d25366339cc7cc6b9c6a2c9
boska <me@boska.space>
2019-03-31 16:20:16 +0800

    Add carthage dependencies option with Xcodegen
    - project.yml is for Xcodegen to regenerate xcodeproj
    - Network Platform Only, can't solve QueryKit with carthage

--- CleanArchitectureRxSwift/Application/Application.swift
+++ CleanArchitectureRxSwift/Application/Application.swift
@@ -1,39 +1,39 @@
 import Foundation
 import Domain
 import NetworkPlatform
-import CoreDataPlatform
-import RealmPlatform
+//import CoreDataPlatform
+//import RealmPlatform
 
 final class Application {
     static let shared = Application()
 
-    private let coreDataUseCaseProvider: Domain.UseCaseProvider
-    private let realmUseCaseProvider: Domain.UseCaseProvider
+//    private let coreDataUseCaseProvider: Domain.UseCaseProvider
+//    private let realmUseCaseProvider: Domain.UseCaseProvider
     private let networkUseCaseProvider: Domain.UseCaseProvider
 
     private init() {
-        self.coreDataUseCaseProvider = CoreDataPlatform.UseCaseProvider()
-        self.realmUseCaseProvider = RealmPlatform.UseCaseProvider()
+//        self.coreDataUseCaseProvider = CoreDataPlatform.UseCaseProvider()
+//        self.realmUseCaseProvider = RealmPlatform.UseCaseProvider()
         self.networkUseCaseProvider = NetworkPlatform.UseCaseProvider()
     }
 
     func configureMainInterface(in window: UIWindow) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let cdNavigationController = UINavigationController()
-        cdNavigationController.tabBarItem = UITabBarItem(title: "CoreData",
-                image: UIImage(named: "Box"),
-                selectedImage: nil)
-        let cdNavigator = DefaultPostsNavigator(services: coreDataUseCaseProvider,
-                navigationController: cdNavigationController,
-                storyBoard: storyboard)
-
-        let rmNavigationController = UINavigationController()
-        rmNavigationController.tabBarItem = UITabBarItem(title: "Realm",
-                image: UIImage(named: "Toolbox"),
-                selectedImage: nil)
-        let rmNavigator = DefaultPostsNavigator(services: realmUseCaseProvider,
-                navigationController: rmNavigationController,
-                storyBoard: storyboard)
+//        let cdNavigationController = UINavigationController()
+//        cdNavigationController.tabBarItem = UITabBarItem(title: "CoreData",
+//                image: UIImage(named: "Box"),
+//                selectedImage: nil)
+//        let cdNavigator = DefaultPostsNavigator(services: coreDataUseCaseProvider,
+//                navigationController: cdNavigationController,
+//                storyBoard: storyboard)
+//
+//        let rmNavigationController = UINavigationController()
+//        rmNavigationController.tabBarItem = UITabBarItem(title: "Realm",
+//                image: UIImage(named: "Toolbox"),
+//                selectedImage: nil)
+//        let rmNavigator = DefaultPostsNavigator(services: realmUseCaseProvider,
+//                navigationController: rmNavigationController,
+//                storyBoard: storyboard)
 
         let networkNavigationController = UINavigationController()
         networkNavigationController.tabBarItem = UITabBarItem(title: "Network",
@@ -45,14 +45,14 @@ final class Application {
 
         let tabBarController = UITabBarController()
         tabBarController.viewControllers = [
-                cdNavigationController,
-                rmNavigationController,
+//                cdNavigationController,
+//                rmNavigationController,
                 networkNavigationController
         ]
         window.rootViewController = tabBarController
 
-        cdNavigator.toPosts()
-        rmNavigator.toPosts()
+//        cdNavigator.toPosts()
+//        rmNavigator.toPosts()
         networkNavigator.toPosts()
     }
 }
